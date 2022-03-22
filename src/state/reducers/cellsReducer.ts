74a39a334e12aa4f3ec555f60665623c3c1854bd
Gaurav Rawal <gauravrawal.dev@gmail.com>
2022-03-22 23:25:55 +0530

    added cells code

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -23,22 +23,27 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
+
       state.data[id].content = content;
+
       return state;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
+
       return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
         return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
+
       return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
