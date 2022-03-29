6c5b406d695115e57a8afdf6a6c1f4319066785c
Gaurav Rawal <gauravrawal.dev@gmail.com>
2022-03-29 22:53:39 +0530

    added local-api

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log('Server is listening');
+exports.serve = void 0;
+const serve = (port, filename, dir) => {
+    console.log(port, filename, dir);
 };
+exports.serve = serve;
